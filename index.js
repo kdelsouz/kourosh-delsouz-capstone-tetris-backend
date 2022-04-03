be98fff758505ec66cb97169155d953678c3da10
Kourosh Delsouz <kdel2008@gmail.com>
2022-04-03 16:57:09 -0700

    added cors to backend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 5050;
 
 const scoreRoutes = require('./routes/scoreRoute');
 
+app.use(cors());
 app.use('/scores', scoreRoutes);
 
 app.listen(PORT, () => {
